b6b4f75d7cf033a35da2035a27857c59a8d17ade
Suleman <149071013+sulemvn7@users.noreply.github.com>
2025-12-08 11:55:10 +0000

    Update insert_test_data.sql

--- insert_test_data.sql
+++ insert_test_data.sql
@@ -9,3 +9,13 @@ INSERT INTO appointments (name, email, date, time)
 VALUES ('Alice Johnson', 'alice@example.com', '2025-11-20', '10:00:00'),
        ('Bob Smith', 'bob@example.com', '2025-11-21', '14:30:00'),
        ('Charlie Brown', 'charlie@example.com', '2025-11-22', '09:15:00');
+
+-- Insert Default user (username: gold, password: smiths)
+INSERT INTO users (username, first, last, email, hashedPassword)
+VALUES (
+    'gold',
+    'Gold',
+    'Smith',
+    'gold@example.com',
+    '$2b$10$Zf2f8okX6wJpVCft56Er/uW.Qr7D5EKOHqMriaDIZLCFwlpQJw/QG'
+);
